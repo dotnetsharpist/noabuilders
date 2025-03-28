74e6e535a9918bf208a1ecd1e9a0fa49918f8092
sharifov-aslbek <sharifovaslbek5@gmail.com>
2025-03-28 14:19:29 +0500

    dd

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,6 @@
   
     </div>
 
-    <!-- <button class="hidden md:block rounded-[4px] bg-black text-[#DFBA7F] px-4 py-2 text-sm">{{ $t('login') }}</button> -->
     <button class="md:hidden cursor-pointer" @click="mobileMenuOpen = !mobileMenuOpen">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
